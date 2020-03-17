a520d8a33ea3dc9135ca7280c1cba21fa03850c2
Rajat Cambo <rajatcambo7@gmail.com>
2020-03-18 01:32:59 +0530

    useEffect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,30 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import classes from './Cockpit.css'
 
 const cockpit = props => {
-    let btnClass=''
+  useEffect(() => {
+    console.log("[Cockpit.js] useEffect")
+    //to pretend an http req using setTimeout
+    setTimeout(() => {
+      alert('Data saved to cloud sucessfully')
+    }, 1000)
+  }, [])
+  //second argument of useEffect takes array of elements for which it should execute
+  //if empty, only runs once
 
-    if(props.showPersons){
-        btnClass = classes.red
-    }
+  let btnClass=''
 
-    let assignedClasses=[]
-    if(props.persons.length <= 2){
-      assignedClasses.push(classes.red)
-    }
-    if(props.persons.length <= 1){
-      assignedClasses.push(classes.bold)
-    }
+  if(props.showPersons){
+      btnClass = classes.red
+  }
 
-    return (
-        <div className={classes.Cockpit}>
-            <h1>{props.title}</h1>
-            <p className={assignedClasses.join(' ')}>hola como estas !</p>
-            <button className={btnClass} onClick={props.clicked}>
-            Toggle Persons
-            </button>
-        </div>
-    )
+  let assignedClasses=[]
+  if(props.persons.length <= 2){
+    assignedClasses.push(classes.red)
+  }
+  if(props.persons.length <= 1){
+    assignedClasses.push(classes.bold)
+  }
+
+  return (
+      <div className={classes.Cockpit}>
+          <h1>{props.title}</h1>
+          <p className={assignedClasses.join(' ')}>hola como estas !</p>
+          <button className={btnClass} onClick={props.clicked}>
+          Toggle Persons
+          </button>
+      </div>
+  )
 }
 
 export default cockpit
