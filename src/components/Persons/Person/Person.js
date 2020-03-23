bec8d130ebdbc880659d06a6836552977e40cf48
Rajat Cambo <rajatcambo7@gmail.com>
2020-03-23 23:22:00 +0530

    auxiliary component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
+import Aux from '../../hoc/Auxiliary'
 import classes from './Person.css'
 
 class Person extends Component {
     render() {
         console.log("[Person.js] rendering...")
         return (
-            <div className={classes.Person}>
+            //we can also use React.Fragment instead of Aux component
+            <Aux>
                 <p onClick={this.props.click}>this is {this.props.name} of {this.props.age} years old</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.nameChange} value={this.props.name} />
-            </div>
+            </Aux>
         )
     }
 }
